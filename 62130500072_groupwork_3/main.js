465d0c0cad56713156291de289d95ebd8f4e3341
Pattarapol Sangsunee <Pattarapol.plasma@mail.kmutt.ac.th>
2021-02-10 13:10:00 +0700

    [UPDATE]info

--- 62130500072_groupwork_3/main.js
+++ 62130500072_groupwork_3/main.js
@@ -1,9 +1,9 @@
 const app = {
     data() {
         return {
-            tasks: [{info: 'Learing INT203 Client-side Web Programming 2', Image: './images/cityInAfterNoon.jpg',done: false},
-                    {info: 'Building modern websites with tailwindcss', Image: './images/cityInSunset.jpg' ,done: false},
-                    {info: 'Developer web applications fast and hassle free with Spring', Image: './images/cityInNight.jpg' ,done: false}
+            tasks: [{info: 'In the afternoon', Image: './images/cityInAfterNoon.jpg',done: false},
+                    {info: 'Empire state and the sunset', Image: './images/cityInSunset.jpg' ,done: false},
+                    {info: 'Street in small town', Image: './images/cityInNight.jpg' ,done: false}
                 ]
         }
     },
