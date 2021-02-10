db48b31a8d69900e34b69a09e6107a3c7d193993
Pattarapol Sangsunee <Pattarapol.plasma@mail.kmutt.ac.th>
2021-02-10 12:58:34 +0700

    [UPDATE]FavButton

--- 62130500072_groupwork_3/main.js
+++ 62130500072_groupwork_3/main.js
@@ -1,9 +1,9 @@
 const app = {
     data() {
         return {
-            tasks: [{title: 'Learing INT203 Client-side Web Programming 2', Image: './images/cityInAfterNoon.jpg',done: false},
-                    {title: 'Building modern websites with tailwindcss', Image: './images/cityInSunset.jpg' ,done: false},
-                    {title: 'Developer web applications fast and hassle free with Spring', Image: './images/cityInNight.jpg' ,done: false}
+            tasks: [{info: 'Learing INT203 Client-side Web Programming 2', Image: './images/cityInAfterNoon.jpg',done: false},
+                    {info: 'Building modern websites with tailwindcss', Image: './images/cityInSunset.jpg' ,done: false},
+                    {info: 'Developer web applications fast and hassle free with Spring', Image: './images/cityInNight.jpg' ,done: false}
                 ]
         }
     },
@@ -16,6 +16,7 @@ const app = {
         countUndone(){
             return this.tasks.filter( t => !t.done ).length
         }
-    }
+        
+}
 }
 Vue.createApp(app).mount('#app')
